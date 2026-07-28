f42793b51058a653791b205b523bf9ea452dc372
kkllchkv <kkrllchernikov@yandex.ru>
2026-07-29 02:15:03 +0300

    Kirill C: ДЗ по уроку №8

---
+++ homework/kirill_chernikov/homework_8/task_2.py
@@ -0,0 +1,42 @@
+# Напишите функцию-генератор, которая генерирует бесконечную последовательность чисел фибоначчи
+# Распечатайте из этого списка пятое число, двухсотое число, тысячное число, стотысячное число
+
+global_iteration_counter = 1
+
+def fibonacci_numbers():
+    counter = 1
+    numbers = [0, 1]
+
+    while counter != 100004:
+        numbers.append(numbers[-1] + numbers[-2])
+        yield numbers[-1]
+        counter += 1
+
+
+"""
+Интернеты подсказали вот такую реализацию
+Решил закрепить написанием 
+
+def fibonacci_numbers():
+    a, b = 1, 1
+    yield a
+    yield b
+
+    while True:
+        a, b = b, a + b
+        yield b
+        
+iteration_counter = 0
+"""
+
+for number in fibonacci_numbers():
+    global_iteration_counter += 1
+    if (global_iteration_counter == 5 or global_iteration_counter == 200
+            or global_iteration_counter == 1000 or global_iteration_counter == 100000):
+        print(number)
+
+    if global_iteration_counter > 100000:
+        break
+
+
+
