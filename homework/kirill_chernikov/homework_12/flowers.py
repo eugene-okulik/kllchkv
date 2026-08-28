b6b1b5218eae1b385fce32220bb8980765b79292
kkllchkv <kkrllchernikov@yandex.ru>
2026-08-28 23:22:24 +0300

    Kirill C: ДЗ №12. Поправил lint

--- homework/kirill_chernikov/homework_12/flowers.py
+++ homework/kirill_chernikov/homework_12/flowers.py
@@ -6,19 +6,23 @@ class Flower:
         self.vase_life = vase_life
         self.cost = cost
 
+
 class Rose(Flower):
     def __init__(self, name, color, height, vase_life, cost, has_thorns):
         super().__init__(name, color, height, vase_life, cost)
         self.has_thorns = has_thorns
 
+
 class Chamomile(Flower):
     def __init__(self, name, color, height, vase_life, cost):
         super().__init__(name, color, height, vase_life, cost)
+
 
 class Orchid(Flower):
     def __init__(self, name, color, height, vase_life, cost, flower_type):
         super().__init__(name, color, height, vase_life, cost)
         self.type = flower_type
+
 
 rose = Rose('Роза', 'Красный', 30, 3, 100,True)
 chamomile = Chamomile('Ромашка', 'Белый', 10, 1, 15)
@@ -30,7 +34,6 @@ class Bouquet:
         self.flowers = flowers
         self.order_flowers = order_flowers
 
-
     def calculate_cost(self):
         total_cost = 0
 
@@ -39,26 +42,20 @@ class Bouquet:
 
         return total_cost
 
-
     def calculate_days_lifespan(self):
         return sum([flower.vase_life for flower in self.order_flowers]) / len(self.order_flowers)
-
 
     def sort_by_vase_life(self):
         return sorted(self.flowers, key=lambda x: x.vase_life)
 
-
     def sort_by_name(self):
         return sorted(self.flowers, key=lambda x: x.name)
-
 
     def sort_by_color(self):
         return sorted(self.flowers, key=lambda x: x.color)
 
-
     def sort_by_cost(self):
         return sorted(self.flowers, key=lambda x: x.cost)
-
 
     def find_by_param(self, param, param_name):
         for flower in self.flowers:
@@ -67,11 +64,9 @@ class Bouquet:
 
         return None
 
-
     def find_by_name(self, name):
         flower = self.find_by_param(name, 'name')
         return flower
-
 
     def find_by_height(self, height):
         flower = self.find_by_param(height, 'height')
@@ -92,4 +87,3 @@ print(t_cost)
 print(new_flower)
 print(sort_flowers_vase_life)
 print(sort_flowers_by_name)
-
